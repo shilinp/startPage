6bf9dad9620dceb65841e017c8a544b8e572387a
shilinp <shilin@ucla.edu>
2017-02-06 21:57:54 -0800

    Added rough implementation of history feature in comments.

--- Assets/functional.js
+++ Assets/functional.js
@@ -1,18 +1,55 @@
-var array = [];
+var allowHistory = 0;
 
 function defaults() {
-    if (localStorage.getItem("array") === null) {
-        array.push(["g", "https://www.google.com", "https://www.google.com/#q="]);
-        array.push(["r", "https://www.reddit.com", "https://www.reddit.com/r/"]);
-        array.push(["y", "https://www.youtube.com", "https://www.youtube.com/results?search_query="]);
-        array.push(["t", "https://www.twitter.com", "https://www.twitter.com/search?q="]);
-        array.push(["4", "https://www.4chan.org", "https://www.4chan.org/"]);
-        array.push(["w", "https://www.wolframalpha.com", "https://www.wolframalpha.com/input/?i="]);
-        array.push(["s", "https://soundcloud.com", "https://soundcloud.com/search?q="]);
-        array.push(["tr", "https://www.tumblr.com", "https://www.tumblr.com/search/"]);
-        localStorage.setItem("array", JSON.stringify(array));
+    var arr = [];
+   //  var hist = [];
+    //
+   //  if (localStorage.getItem("hist") === null) {
+   //      localStorage.setItem("hist", JSON.stringify(hist));
+   //  }
+
+    if (localStorage.getItem("arr") === null) {
+        arr.push(["g", "https://www.google.com", "https://www.google.com/#q="]);
+        arr.push(["r", "https://www.reddit.com", "https://www.reddit.com/r/"]);
+        arr.push(["y", "https://www.youtube.com", "https://www.youtube.com/results?search_query="]);
+        arr.push(["t", "https://www.twitter.com", "https://www.twitter.com/search?q="]);
+        arr.push(["4", "https://www.4chan.org", "https://www.4chan.org/"]);
+        arr.push(["w", "https://www.wolframalpha.com", "https://www.wolframalpha.com/input/?i="]);
+        arr.push(["s", "https://soundcloud.com", "https://soundcloud.com/search?q="]);
+        arr.push(["tr", "https://www.tumblr.com", "https://www.tumblr.com/search/"]);
+        localStorage.setItem("arr", JSON.stringify(arr));
     }
+
+   //  if (localStorage.getItem("check") == 1) {
+   //      document.getElementById("checkbox").checked = true;
+   //      allowHistory = 1;
+   //  }
 }
+
+// function historyrecord(checkbox) {
+//     if (checkbox.checked) {
+//         allowHistory = 1;
+//         localStorage.setItem("check", 1);
+//     }
+// }
+//
+// function historyfunc() {
+//     var hist = JSON.parse(localStorage.getItem("hist"));
+//     if (allowHistory == 0) {
+//         document.getElementById("historyBox").innerHTML = "You have disabled history!"
+//     } else if (hist == "") {
+//         document.getElementById("historyBox").innerHTML = "You haven't visited any pages yet!"
+//     } else if (document.getElementById("historyBox").innerHTML == "") {
+//         for (i = 0; i < hist.length; i++) {
+//             var x = i + 1;
+//             document.getElementById("historyBox").innerHTML += "<tr><td class = \"macroNumber\">" + x + "</td>";
+//             document.getElementById("historyBox").innerHTML += "<td class = \"macroLetter\">" + "-" + hist[i] + "</td></tr></br>";
+//         }
+//     } else {
+//         document.getElementById("historyBox").innerHTML = "";
+//     }
+// }
+
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-
 VISUAL DATA
 -=-=-=-=-=-=-=-=-=-=-=-=-=-*/
@@ -29,13 +66,13 @@ function loadBack() {
 }
 
 function updateMacroTable() {
-    var temp = JSON.parse(localStorage.getItem("array"));
-    for (i = 0; i < temp.length; i++) {
+    var arr = JSON.parse(localStorage.getItem("arr"));
+    for (i = 0; i < arr.length; i++) {
         var x = i + 1;
-        document.write("<tr><td class = \"macroNumber\">" + x + "</td>")
-        document.write("<td class = \"macroLetter\">" + "-" + temp[i][0] + "</td>");
-        document.write("<td class = \"macroURL\">" + temp[i][1] + "</td>")
-        document.write("<td class = \"macroURL\">" + temp[i][2] + "</td></tr>")
+        document.write("<tr><td class = \"macroNumber\">" + x + "</td>");
+        document.write("<td class = \"macroLetter\">" + "-" + arr[i][0] + "</td>");
+        document.write("<td class = \"macroURL\">" + arr[i][1] + "</td>");
+        document.write("<td class = \"macroURL\">" + arr[i][2] + "</td></tr>");
     }
 }
 
@@ -92,14 +129,11 @@ function checkTime(i) {
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-
 TEXT DATA
 -=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-function localRetrieve() //Place the cached array into the global array variable
-{
-    array = JSON.parse(localStorage.getItem("array"));
-}
 
 function store() //Stores a website shortcut
 {
-    localRetrieve();
+    var arr = JSON.parse(localStorage.getItem("arr"));
+
     var charInput = document.getElementById("character_enter").value;
     document.getElementById("character_enter").value = "";
     var webURL = document.getElementById("url_enter").value;
@@ -107,56 +141,64 @@ function store() //Stores a website shortcut
     var queryURL = document.getElementById("search_enter").value;
     document.getElementById("search_enter").value = "";
     var bool;
-    for (i = 0; i < array.length; i++) {
-        if (charInput == array[i][0]) {
+    for (i = 0; i < arr.length; i++) {
+        if (charInput == arr[i][0]) {
             bool = 1;
         }
     }
     if (bool != 1) {
-        array.push([charInput, webURL, queryURL]);
+        arr.push([charInput, webURL, queryURL]);
         location.reload(true);
     } else {
         alert("That Macro ID is already taken!")
     }
-    localStorage.setItem("array", JSON.stringify(array));
+    localStorage.setItem("arr", JSON.stringify(arr));
 }
 
 function clearSites() {
-    localStorage.removeItem("array");
+    localStorage.removeItem("arr");
     defaults();
     location.reload(true);
 }
 
+// function clearHistory() {
+//     localStorage.removeItem("hist");
+//     defaults();
+//     location.reload(true);
+// }
+
 function clearIndividual() {
-    localRetrieve();
+    var arr = JSON.parse(localStorage.getItem("arr"));
+
     var x = document.getElementById("clear_one").value;
     document.getElementById("clear_one").value = "";
     var v = parseInt(x, 10);
     var y = [];
     for (i = 0;
-        (i + v) < array.length; i++) {
-        y.unshift(array[array.length - i - 1]);
-        array.pop();
+        (i + v) < arr.length; i++) {
+        y.unshift(arr[arr.length - i - 1]);
+        arr.pop();
     }
-    array.pop();
+    arr.pop();
     for (i = 0; i < y.length; i++) {
-        array.push(y[i]);
+        arr.push(y[i]);
     }
-    localStorage.setItem("array", JSON.stringify(array));
+    localStorage.setItem("arr", JSON.stringify(arr));
     location.reload(true);
 }
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-
 SEARCH
 -=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-function find(userInput) //Returns position of array in the array has a character that matches the 0 position of userInput
+function find(userInput) //Returns position of arr in the arr has a character that matches the 0 position of userInput
 {
-    for (i = 0; i < array.length; i++) {
-        if (userInput.charAt(1) != userInput.charAt(array.length - 1) && userInput.charAt(2) != " ") {
-            if (array[i][0].charAt(0) == userInput.charAt(1) && array[i][0].charAt(1) == userInput.charAt(2)) {
+    var arr = JSON.parse(localStorage.getItem("arr"));
+    for (i = 0; i < arr.length; i++) {
+        if (userInput.charAt(1) != userInput.charAt(arr.length - 1) && userInput.charAt(2) != " ") {
+            if (arr[i][0].charAt(0) == userInput.charAt(1) && arr[i][0].charAt(1) == userInput.charAt(2)) {
                 return i;
             }
-        } else if (userInput.charAt(1) == userInput.charAt(array.length - 1) || userInput.charAt(2) == " ") {
-            if (array[i][0] == userInput.charAt(1)) {
+        } else if (userInput.charAt(1) == userInput.charAt(arr.length - 1) || userInput.charAt(2) == " ") {
+            if (arr[i][0] == userInput.charAt(1)) {
                 return i;
             }
         }
@@ -166,23 +208,38 @@ function find(userInput) //Returns position of array in the array has a characte
 
 function search() //Action command to search the internet
 {
-    localRetrieve();
+    var arr = JSON.parse(localStorage.getItem("arr"));
+   //  var hist = JSON.parse(localStorage.getItem("hist"));
+
     var userInput = document.getElementById("userInput").value;
+
     if (userInput.charAt(0) == "-") {
         var num = find(userInput);
         if (num != -1) {
             if (userInput.charAt(2) == " " || userInput.charAt(3) == " ") {
-                var query = array[num][2];
+                var query = arr[num][2];
                 var site = query.concat(userInput.substr(userInput.indexOf(" ") + 1));
+               //  if (allowHistory == 1) {
+               //      hist.push(arr[num][1] + " Search: " + userInput.substr(userInput.indexOf(" ") + 1));
+               //      localStorage.setItem("hist", JSON.stringify(hist));
+               //  }
                 window.location.href = site;
             } else {
-                window.location.href = array[num][1];
+               //  if (allowHistory == 1) {
+               //      hist.push(arr[num][1]);
+               //      localStorage.setItem("hist", JSON.stringify(hist));
+               //  }
+                window.location.href = arr[num][1];
             }
         }
     } else {
         var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
         var regex = new RegExp(expression);
         if (userInput.match(regex)) { //Test for url
+            // if (allowHistory == 1) {
+            //     hist.push(userInput);
+            //     localStorage.setItem("hist", JSON.stringify(hist));
+            // }
             if (!userInput.match(/^https?:\/\//i)) {
                 userInput = 'http://' + userInput;
             }
@@ -190,6 +247,10 @@ function search() //Action command to search the internet
         } else { //Default search Google
             var google = "https://www.google.com/search?q=";
             var search = google.concat(userInput);
+            // if (allowHistory == 1) {
+            //     hist.push(userInput);
+            //     localStorage.setItem("hist", JSON.stringify(hist));
+            // }
             window.location.href = search;
         }
     }
